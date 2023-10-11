d9db391b3a5bf0c7b35f9a22fe60711ab4c72bd9
salvadorkano <gorikano@gmail.com>
2023-10-11 01:15:14 -0600

    Add nodemon and gitignore

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # backRedTec
 
 This is the backend project to create posts to the alumns of the Tec school
+
+# Investigar
+
+Investigar un provedor de correos
+
+# Definir lo que usaremos
+
+Para que el codigo quede mas limpio usaremos prettier
+Para la base de datos usaremos mongoDB
+Para desplegar vercel
+
+# Definir login
+
+Vamos usar un login y se usara con JWT POST que tome el correo y la password y lo verifique y retorne un 200 y que regrsese informacion del usuario como nombre, numero de control, token, etc
+
+Para recuperar contarseña necesitamos un servicio POST que tome el correo y envie para verificar o cambiar la contarseña
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Ya jala");
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+const express = require("express");
+const app = express();
+const port = 3000;
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
